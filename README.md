8f1887dbff0791fddfbd343ce45a9d14d776e6f5
Adrian <adrianbailador@hotmail.com>
2020-10-28 19:40:12 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# apiGestores
+ Creating and connecting a CRUD with API Rest using React JS and ASP.NET Core.
---
+++ apiGestores/Controllers/GestoresController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using apiGestores.Context;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace apiGestores.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GestoresController : Controller
+    {
+        private readonly AppDbContext context; 
+
+        public GestoresController(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        // GET: api/<GestoresController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<GestoresController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<GestoresController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<GestoresController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<GestoresController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
