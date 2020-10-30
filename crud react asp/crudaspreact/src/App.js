36809f933eedef4bb70652820bbf924aa7430eb6
Adrian <adrianbailador@hotmail.com>
2020-10-30 21:26:48 +0000

    Update App.js
    Metodo insertar con un Modal

--- crud react asp/crudaspreact/src/App.js
+++ crud react asp/crudaspreact/src/App.js
@@ -9,12 +9,44 @@ function App() {
 
   const baseUrl="https://localhost:44341/api/gestores";
   const [data, setData]=useState([]);
+  const [modalInsertar, setModalInsertar]=useState(false);
+  const [gestorSeleccionado, setGestorSeleccionado]=useState({
+    id: '',
+    nombre: '',
+    lanzamiento: '',
+    desarrollador: ''
+  })
 
+  //Metodo para capturar lo que el user escribe en los inputs
+  const handleChange=e=>{
+    const {name,value}=e.target;
+    setGestorSeleccionado({
+      ...gestorSeleccionado,
+      [name]: value
+    });
+    console.log(gestorSeleccionado);
+  }
+
+  const abrirCerrarModalInsertar=()=>{
+    setModalInsertar(!modalInsertar);
+  }
 
   const peticionGet=async()=>{
     await axios.get(baseUrl)
      .then(response=>{
         setData(response.data);
+     }).catch(error=>{
+        console.log(error);
+    })
+  }
+
+  const peticionPost=async()=>{
+    delete gestorSeleccionado.id; //Eliminamos el atributo id
+    gestorSeleccionado.lanzamiento=parseInt(gestorSeleccionado.lanzamiento);
+    await axios.post(baseUrl,gestorSeleccionado)
+     .then(response=>{
+        setData(data.concat(response.data));
+        abrirCerrarModalInsertar()
      }).catch(error=>{
         console.log(error);
     })
@@ -26,6 +58,9 @@ function App() {
 
   return (
     <div className="App">
+      <br/><br/>
+      <button onClick={()=>abrirCerrarModalInsertar()} className="btn btn-success">Insertar Nuevo Gestor</button>
+      <br/><br/>
      <table className="table table-bordered">
        <thead>
          <tr>
@@ -51,8 +86,41 @@ function App() {
         ))}
        </tbody>
      </table>
+
+
+
+  
+
+
+     <Modal isOpen={modalInsertar}>
+       <ModalHeader>
+         Insertar Gestor de Base de Datos</ModalHeader>
+         <ModalBody>
+           <div className="form-group">
+             <label>Nombre: </label>
+             <br />
+             <input type="text" className="form-group" name="nombre" onChange={handleChange}/>
+             <br />
+             <label>Lanzamiento: </label>
+             <br />
+             <input type="text" className="form-group" name="lanzamiento" onChange={handleChange}/>
+             <br />
+             <label>Desarrollador: </label>
+             <br />
+             <input type="text" className="form-group" name="desarrollador" onChange={handleChange}/>
+             <br />
+           </div>
+         </ModalBody>
+         <ModalFooter>
+           <button className="btn btn-primary" onClick={()=>peticionPost()}>Insertar</button>{"   "}
+           <button className="btn btn-danger" onClick={()=>abrirCerrarModalInsertar()}>Cancelar</button> {"  "}
+         </ModalFooter>
+     </Modal>
     </div>
   );
 }
+
+
+
 
 export default App;
